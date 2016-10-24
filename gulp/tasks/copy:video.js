805d1d712f632d5eb1649bede374f231a69b5713
Stas <Cantanadp@gmail.com>
2016-10-24 09:51:59 +0300

    Разметка футера и стилизация

--- gulp/tasks/copy:video.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by max on 30.09.16.
- */
-module.exports = function() {
-    $.gulp.task('copy:video', function() {
-        return $.gulp.src('./source/video/*.*')
-            .pipe($.gulp.dest($.config.root+'/assets/video/'));
-    });
-};
